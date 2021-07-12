480e25bf0861712c9012d31d8d7cbd42ddd9027e
Vlad <Vladpetr73@gmail.com>
2021-07-13 02:49:23 +0300

    Последняя версия программы с добавленной функцией:
    1.Чтения из файла

---
+++ lab11/List.h
@@ -0,0 +1,34 @@
+#ifndef __LIST__H__
+#define __LIST__H__
+
+// Структура - элемент списка
+struct SItem
+{
+	int Id;               // порядковый номер студента
+	char Name[20];        // ФИО студента
+	char Surname[20]; 
+	char Otche[20];
+	char Number[20]; // номер телефона
+	SItem *next;
+};
+
+namespace Petrovich
+{
+	// =========ПРОТОТИПЫ ФУНКЦИЙ======================
+	// Выводит на экран все элементы списка
+	void OutList(SItem *Head);
+	// Добавляет элемент в конец списка
+	void AddItem(SItem *Head, SItem *NewItem, SItem **Teil);
+	// Удаляет элемент списка с идентификатором ID. Возвращает 
+	// адрес удаленного элемента или NULL, если элемент не найден.
+	SItem *DeleteItem(SItem *Head, int Id, SItem **Teil);
+	// Находит элемент списка с идентификатором ID. Возвращает 
+	// адрес найденного элемента или NULL, если элемент не найден.
+	SItem *GetItem(SItem *Head, int Id);
+	// Полное удаление элементов списка
+	void DeleteAllItem(SItem **Head, SItem **Teil);
+	// Вставляет элемент списка в лексиграфическом порядке
+	void AddItemInside(SItem *Head, SItem *NewItem, SItem **Teil);
+	
+}
+#endif
---
+++ lab11/lab11.cpp
@@ -0,0 +1,216 @@
+// lab11.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include "iostream"
+#include "conio.h"
+#include "List.h"
+#include <fstream>
+#include <iostream>
+#include <cstringt.h>
+#include <iomanip>
+
+using namespace std;
+using namespace Petrovich;
+
+// элементы меню
+enum menu {EXIT, NEW_ELEM, DELETE_ELEM, DELETE_ALL, PRINT_ELEM, PRINT_ALL, SAVE_FILE, READ_FILE, END};
+
+int Menu ();
+void _tmain ()
+{
+	setlocale(LC_CTYPE, "Russian_Russia.1251");
+
+	SItem *Head = NULL;		// указатель на начало списка (голова)
+	SItem *Teil;			// указатель на конец списка (хвост)
+	int *Id;				// указатель на порядковый номер студента
+	SItem *Buff;			// буфер
+	SItem *NewItem;			// новый элемент списка
+	int CmdMenu;			// команды меню
+	int i;					// счетчик
+	char NameFileTxt[100] ; // для текстового файла записи и чтения
+	
+	OutList(Head); //вывод списка
+
+	// Создание головного элемента списка
+	Head    = new SItem;
+	Head->Id   = 0;
+	Head->next = NULL;
+	Teil = Head;
+
+	//создаем объект file для записи в текстовый файл
+	ofstream file;
+	//создаем объект Readfile для чтения из текстового файла
+	ifstream read_file;
+	//создаем объект Writefile для записи в текстовый файл
+	ofstream write_file;
+	
+	// для команд меню
+	for (;;)
+	{
+	CmdMenu = Menu (); 
+	switch (CmdMenu)
+	{
+		// выход
+		case EXIT:
+			return;
+			break;
+		// добавляет новый элемент
+		case NEW_ELEM:
+			NewItem = new SItem;
+			cout << "Студент номер >> ";
+			cin >> NewItem -> Id ;
+			AddItem(Head, NewItem, &Teil);
+			cout << endl;
+			cout << "Введите фамилию >>";
+			cin >> NewItem -> Surname ;
+			cout << endl;
+			cout << "Введите имя >> ";
+			cin >> NewItem -> Name ;
+			cout << endl;
+			cout << "Введите отчество >> ";
+			cin >> NewItem -> Otche ;
+			cout << endl;
+			cout << "Введите номер телефона >> ";
+			cin >> NewItem -> Number;
+			cout << endl;
+		break;
+		// выводит все элементы	
+		case PRINT_ALL:
+			OutList(Head);
+		break;
+		// выводит выбранный элемент
+		case PRINT_ELEM:
+			Id = new int;
+			cout << "Номер студента >> ";
+			cin >> *Id;
+			Buff = GetItem(Head, *Id);
+			if(Buff)
+			{
+			cout << "Найден элемент Id = " << setw(2) << Buff->Id;
+			cout << setw(13) << Buff->Surname << setw(13) << Buff->Name;
+			cout << setw(13) << Buff->Otche << setw(13)<< Buff ->Number << endl;
+			}
+			else
+				cout << "Элемент не найден." << endl;
+		break;
+		// удаляет выбранный элемент
+        case DELETE_ELEM:
+			Id = new int;
+            cout << "Введите номер студента >> ";
+            cin >> *Id;
+            DeleteItem(Head, *Id, &Teil);
+            cout << "Список с удаленным элементом" << endl;
+			// удаляет все элементы
+        case DELETE_ALL:
+            DeleteAllItem(&Head, &Teil);
+			// сохраняет список в текстовый файл
+		case SAVE_FILE:
+			cout << "Введите файл . txt для хранения списка >> ";
+			cin >> NameFileTxt;              // название текстового файла
+			// открываем файл и проверяем на ошибки
+			file.open(NameFileTxt, ios::app);
+				if (!file.is_open())
+				{
+					cout<<"Ошибка открытия файла \""<<NameFileTxt;
+					cout<<"\""<<endl;
+					cout<<"Программа работу закончила."<<endl;
+					system("pause");
+					return;
+				}
+			SItem *current;		// указатель на текущий элемент списка
+			if(!Head)
+			{
+				cout << "Список пуст." << endl;
+				return;
+			}
+			current = Head -> next ; // указатель на текущий элемент
+			// записываем в файл элементы списка до тех пор,
+			// пока указатель на следующий элемент не NULL.
+			while (current->next)
+			{
+				// Запись содержимого текущего элемента списка
+				file << current->Id << "\\\\";
+				file << current->Surname << "////" ;
+				file << current->Name << "////" ;
+				file << current->Otche << "////" ;
+				file << current->Number << "////" ;
+				file << endl;
+				// Переходим к следующему элементу списка.
+				current = current->next;	
+			}
+			// Запись содержимого последнего элемента списка
+			// указатель которого на следующий элемент есть NULL.
+			file << current->Id<< "\\\\" << current->Surname<< "////";
+			file << current->Name<< "////"<< current->Otche<< "////" <<current-> Number << "////";
+			file << endl;
+			file.close();
+		break;
+		// чтение элементов списка из файлп
+		case READ_FILE:
+			cout << "Введите файл . txt для чтения списка >> ";
+			cin >> NameFileTxt;
+			read_file.open(NameFileTxt);
+			if (!read_file.is_open())
+			{
+				cout<<"Ошибка открытия файла \""<<NameFileTxt;
+				cout<<"\""<<endl;
+				cout<<"Программа работу закончила."<<endl;
+				system("pause");
+				return;
+			}
+			char *mass;
+			int SizeFile; // Для размера файла
+			read_file.seekg (0, ios::end); // Ставим курсор в начало файла
+			SizeFile = read_file.tellg(); // Определение размера файла
+			read_file.seekg(0, ios::beg); // Возвращаем курсор в начало файла.
+			mass = new char [SizeFile]; // Динамическе выделение памяти для массива list
+			int SizeProgram=0;
+			while(!read_file.eof())
+				read_file >> mass[SizeProgram++];
+			SizeProgram--;
+			for(i=0;i<SizeFile;i++)
+			{
+				read_file.getline(mass, SizeFile);
+				cout<< mass[i];
+			}
+			cout<<endl;
+			// Устанавливаем NULL в конце строки
+			mass[SizeProgram] = NULL;
+			cout << mass << endl << endl;
+			// Закрываем файл
+			read_file.close();
+			write_file.close();
+			break;
+  	 }
+   }
+}
+// Команды меню
+	int Menu ()
+	{
+		int CmdMenu;
+		 using namespace std;
+
+		 cout << "МЕНЮ: " << endl;
+		 for (;;)
+		 {
+			 cout << EXIT << " - выход" << endl;
+			 cout << NEW_ELEM << " - новый элемент " << endl;
+			 cout << DELETE_ELEM << " - удалить элемент" << endl;
+			 cout << DELETE_ALL << " - удалить весь список" << endl;
+			 cout << PRINT_ELEM << " - вывести на экран элемент..." << endl;
+			 cout << PRINT_ALL << " - вывести весь список на экран" << endl;
+			 cout << SAVE_FILE << " - сохранить в файле..." << endl;
+			 cout << READ_FILE << " - чтение из файла" << endl;
+			 cout << "Номер команды меню >> " ;
+			 cin >> CmdMenu;
+			 if (CmdMenu <0 || CmdMenu >= END)
+			 {
+				 cout << " ОШИБКА - ТАКОЙ КОМАНДЫ НЕТ." << endl;
+				 continue;
+			 }
+			 else
+				 return CmdMenu;
+		 }
+
+		 return CmdMenu;
+	}
